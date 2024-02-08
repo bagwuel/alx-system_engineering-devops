fca06705183e3b32e04b9ab38ee89e8dce0c9614
bagwuel <mbagwu39@gmail.com>
2024-02-08 23:00:53 +0100

    complete all tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,8 @@
 # Set up server configuration with puppet
 file_line{'Set alias name':
   path => '/etc/ssh/ssh_config',
-  line => 'Host 100.25.158.171
-    HostName 100.25.158.171
+  line => 'Host 52.91.123.208
+    HostName 52.91.123.208
     User ubuntu
     IdentityFile ~/.ssh/school
     PasswordAuthentication no',
